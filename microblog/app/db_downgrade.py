1d89e1721b1665dc5fa558a9cd5d1c7e47ef6670
Cristian <kandia88@gmail.com>
2013-04-19 22:54:18 -0400

    Script downgrade

---
+++ microblog/app/db_downgrade.py
@@ -0,0 +1,12 @@
+'''
+Created on 19/04/2013
+
+@author: cristian
+'''
+#!usr/bin/python
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
+print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
