b5bafc0fa6591a22fa02391a6c2ad3194fe2ed20
Cristian <kandia88@gmail.com>
2013-04-21 16:12:35 -0400

    Primer commit del branch postgre

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,14 +1,14 @@
 """
 Sygma_System
 :organization:CRF_Proyect
-:author:Cristian Candia
-:author:Ruth Centurion
-:author:Fernando Saucedo
+@author: Cristian Candia
+@author: Ruth Centurion
+@author: Fernando Saucedo
 
 models.py
 """
 from app import db
-__docformat__ = 'restructuredtext'
+
 ROLE_USER = 0
 ROLE_ADMIN = 1
 """:note: Clase que representa al usuario"""
--- microblog/run.py
+++ microblog/run.py
@@ -1,6 +1,5 @@
 '''
 Created on 14/04/2013
-
 @author: cristian miguel
 '''
 #!usr/lib/python
