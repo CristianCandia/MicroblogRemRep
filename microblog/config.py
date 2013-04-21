1c1829df3f74a708fe43937afa776e686d2503bd
Cristian <kandia88@gmail.com>
2013-04-21 17:51:39 -0400

    Varios commit

--- microblog/config.py
+++ microblog/config.py
@@ -10,5 +10,5 @@ SECRET_KEY = 'you-will-never-guess'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://cristian:useruser:@localhost' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://cristian:useruser:@localhost' + os.path.join(basedir, 'app2.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
