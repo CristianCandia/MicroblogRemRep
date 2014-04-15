2f454302eb37bcc8298510a0ed2f38eb32dbfcb0
Cristian <kandia88@gmail.com>
2014-04-15 12:03:48 -0400

    Commit 3 rama 1

--- microblog/app/testVarios.py
+++ microblog/app/testVarios.py
@@ -1,8 +1,11 @@
-'''
-Created on 05/05/2013
-
-@author: cristian
-'''
+"""
+    Sigma_System
+    @organization:CRF_Proyect
+    @authors:
+        - U{Cristian Candia<mailto:kandia88@gmail.com>}
+        - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+        - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
 
 import unittest
 
