2e42752c760950e09e94fb3a332d3bb28f0489b7
Cristian <kandia88@gmail.com>
2014-04-15 13:02:52 -0400

    Commit 2 rama3

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -6,6 +6,15 @@
         - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
         - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
 """
+"""
+    Sigma_System
+    @organization:CRF_Proyect
+    @authors:
+        - U{Cristian Candia<mailto:kandia88@gmail.com>}
+        - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+        - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
+
 from flask_mail import Message
 from decorators import async
 from app import mail
