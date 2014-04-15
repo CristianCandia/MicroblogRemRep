b5088b83372e320dbe975c02b1f8eb1a66a19f3f
Cristian <kandia88@gmail.com>
2014-04-15 00:27:42 -0400

    Commite en pr2

--- microblog/app/vista/__init__.py
+++ microblog/app/vista/__init__.py
@@ -6,3 +6,31 @@ from vistaPermiso import *
 from vistaItem import *
 from vistaTI import *
 from vistaAtributoTI import *
+"""
+@note: Clase que define la vista de Item
+@authors:
+    - U{Cristian Candia<mailto:kandia88@gmail.com>}
+    - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+    - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
+"""
+@note: Clase que define la vista de Item
+@authors:
+    - U{Cristian Candia<mailto:kandia88@gmail.com>}
+    - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+    - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
+"""
+@note: Clase que define la vista de Item
+@authors:
+    - U{Cristian Candia<mailto:kandia88@gmail.com>}
+    - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+    - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
+"""
+@note: Clase que define la vista de Item
+@authors:
+    - U{Cristian Candia<mailto:kandia88@gmail.com>}
+    - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+    - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
