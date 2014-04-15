2ac93dd6f638d2fda4d39dd5ea2254b2b4dd5965
Cristian <kandia88@gmail.com>
2014-04-15 13:09:00 -0400

    Commit rama princ

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -6,6 +6,14 @@
         - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
         - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
 """
+"""
+    Sigma_System
+    @organization:CRF_Proyect
+    @authors:
+        - U{Cristian Candia<mailto:kandia88@gmail.com>}
+        - U{Ruth Centurion<mailto:ruthiccr@gmail.com>}
+        - U{Fernando Saucedo<mailto:carlifer.fernando@gmail.com>}
+"""
 
 from flask.ext.wtf import Form, TextField, PasswordField, DateField, IntegerField, BooleanField, RadioField, SelectField, TextAreaField
 
