3124201b844392cdb67639b7aae5282614f3500a
Cristian <kandia88@gmail.com>
2014-04-16 21:47:46 -0400

    commit 2 en el master

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -11,7 +11,6 @@ from flask_mail import Message
 from decorators import async
 from app import mail
 
-
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
